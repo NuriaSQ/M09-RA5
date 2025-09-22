439160efb730e002be37f35d32b18a02f485ea80
Nuria Sy <nusicu@gmail.com>
2025-09-22 23:49:09 +0200

    Treball Finalitzat

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,65 +1,60 @@
 public class Rot13 {
-    public static void main(String[] args) {
-        String xifraRot13 = "Hola qué Tal!";
 
-        String desxifraRot13 = desxifraRot13(xifraRot13);
-        System.out.println(desxifraRot13);
+    public static char[] guardarAbecedariMinuscula = "aàáäbcçdeèéëfghiìíïjklmnñoòóöpqrstuùúüvwxyz".toCharArray();
+    public static char[] guardarAbecedariMajuscula = "AÀÁÄBCÇDEÈÉËFGHIÌÍÏJKLMNÑOÒÓÖPQRSTUÙÚÜVWXYZ".toCharArray();
+
+    public static void main(String[] args) {
+        String cadenaXifrada = "Hola, què tal? Sóc la Núria.";
+        String cadenaDesxifrada = "Óvug, abm ägu? Áxï ug Úçàög.";
+
+        String resultatCadenaXifrada = xifraRot13(cadenaXifrada);
+        String resultatCadenaDesxifrada = desxifraRot13(cadenaDesxifrada);
+
+        System.out.println("Xifrat: " + cadenaXifrada + " => " + resultatCadenaXifrada);
+        System.out.println("Desxifrat: " + cadenaDesxifrada + " => " + resultatCadenaDesxifrada);
     } 
 
-    private static String xifrarLletresMinuscules (String desxifraRot13, char lletra){
-        String abecedariMinuscula = "abcdefghijklmnñopqrstuvwxyzàèìòùáéíóúäëïöüç";
-        char[] guardarAbecedariMinuscula = abecedariMinuscula.toCharArray();
-        for (int index = 0; index < guardarAbecedariMinuscula.length; index++) {
-            if (lletra == guardarAbecedariMinuscula[index]){
-                if(index + 13 >= guardarAbecedariMinuscula.length){
-                    desxifraRot13 +=guardarAbecedariMinuscula[(index + 13) - guardarAbecedariMinuscula.length];
-                } else {
-                    desxifraRot13 += guardarAbecedariMinuscula[index + 13];
-                }
-            }
-        }
-        return desxifraRot13;
-    }
-
-    private static String xifrarLletresMajuscules (String desxifraRot13, char lletra){
-        String abecedariMajuscula = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZÀÈÌÒÙÁÉÍÓÚÄËÏÖÜÇ";
-        char[] guardarAbecedariMajuscula = abecedariMajuscula.toCharArray();
-        for (int index = 0; index < guardarAbecedariMajuscula.length; index++) {
-            if (lletra == guardarAbecedariMajuscula[index]){
-                if(index + 13 >= guardarAbecedariMajuscula.length){
-                    desxifraRot13 += guardarAbecedariMajuscula[(index + 13) - guardarAbecedariMajuscula.length];
-                } else {
-                    desxifraRot13 += guardarAbecedariMajuscula[index + 13];
-                }
-            }
-        }
-        return desxifraRot13;
-    }
-
-    private static String xifrarCaractersEspecials (String desxifraRot13, char lletra){
-        String caractersEspecials = "!¡.:,;_-?¿ ";
-        char[] guardarCaractersEspecials = caractersEspecials.toCharArray();
-        for (int j = 0; j < guardarCaractersEspecials.length; j++) {
-            if(lletra == guardarCaractersEspecials[j]){
-                desxifraRot13 += guardarCaractersEspecials[j];
-            }   
-        }
-        return desxifraRot13;
-    }
-
-    private static String desxifraRot13(String xifraRot13){
-        String desxifraRot13 = "";
-        for (int i = 0; i < xifraRot13.length(); i++) {
-            char lletra = xifraRot13.charAt(i);
-
+    private static String xifraRot13(String cadena){
+        String xifraRot13 = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char lletra = cadena.charAt(i);
             if (Character.isLowerCase(lletra)) {
-                desxifraRot13 = xifrarLletresMinuscules(desxifraRot13, lletra);
+                for (int index = 0; index < guardarAbecedariMinuscula.length; index++) {
+                    if (lletra == guardarAbecedariMinuscula[index]){
+                        if(index + 13 >= guardarAbecedariMinuscula.length) xifraRot13 += guardarAbecedariMinuscula[(index + 13) - guardarAbecedariMinuscula.length];
+                        else xifraRot13 += guardarAbecedariMinuscula[index + 13];  
+                    }
+                }
             } else if (Character.isUpperCase(lletra)) {
-                desxifraRot13 = xifrarLletresMajuscules(desxifraRot13, lletra);
-            } else {
-                desxifraRot13 = xifrarCaractersEspecials(desxifraRot13, lletra);
-            }
-        }
-        return desxifraRot13;
+                for (int index = 0; index < guardarAbecedariMajuscula.length; index++) {
+                    if (lletra == guardarAbecedariMajuscula[index]){
+                        if(index + 13 >= guardarAbecedariMajuscula.length) xifraRot13 += guardarAbecedariMajuscula[(index + 13) - guardarAbecedariMajuscula.length];
+                        else xifraRot13 += guardarAbecedariMajuscula[index + 13];
+                    }
+                }
+            } else xifraRot13 += lletra;
+        } return xifraRot13;
+    }
+
+    private static String desxifraRot13(String cadena){
+        String desxifraRot13 = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char lletra = cadena.charAt(i);
+            if (Character.isLowerCase(lletra)) {
+                for (int index = 0; index < guardarAbecedariMinuscula.length; index++) {
+                    if (lletra == guardarAbecedariMinuscula[index]){
+                        if(index - 13 < 0) desxifraRot13 += guardarAbecedariMinuscula[(index - 13) + guardarAbecedariMinuscula.length];
+                        else desxifraRot13 += guardarAbecedariMinuscula[index - 13];  
+                    }
+                }
+            } else if (Character.isUpperCase(lletra)) {
+                for (int index = 0; index < guardarAbecedariMajuscula.length; index++) {
+                    if (lletra == guardarAbecedariMajuscula[index]){
+                        if(index - 13 < 0) desxifraRot13 += guardarAbecedariMajuscula[(index - 13) + guardarAbecedariMajuscula.length];
+                        else desxifraRot13 += guardarAbecedariMajuscula[index - 13];
+                    }
+                }
+            } else desxifraRot13 += lletra;
+        } return desxifraRot13;
     }
 }
